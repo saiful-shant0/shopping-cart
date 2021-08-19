ae53821ad529bad7416f84ea0bd8be9179153d68
saiful-shant0 <sshanto.cse@gmail.com>
2021-08-19 21:33:24 +0600

    second

--- app.js
+++ app.js
@@ -13,8 +13,31 @@ function updateCaseNumber(product, price, isincreasing) {
     // update Case Price
     const Price = document.getElementById(product + '-price');
     Price.innerText = Number * price;
+    calculateTotal();
 
 }
+
+function getInput(product) {
+    const Input = document.getElementById(product + '-number');
+
+    const Number = parseInt(Input.value);
+    return Number;
+}
+
+function calculateTotal() {
+
+    const phoneTotal = getInput('phone') * 1219;
+    const caseTotal = getInput('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    //update in html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-ammount').innerText = tax;
+    document.getElementById('total').innerText = totalPrice;
+}
+
+
 //phone case increase decrease events
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateCaseNumber('phone', 1219, true);
